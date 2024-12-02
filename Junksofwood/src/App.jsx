2d40868d58ee86188dee578257059feba812c515
Gerogie18 <jennlyver@gmail.com>
2024-12-02 14:32:57 -0330

    Base File Establish
    Establishing a base file

---
+++ Junksofwood/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+        <h1>Sprint App</h1>
+      </div>
+  )
+}
+
+export default App
