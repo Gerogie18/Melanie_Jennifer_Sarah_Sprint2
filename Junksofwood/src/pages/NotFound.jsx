d0bf314aa6d4827278755a5679b0b1bac11504c4
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-12-05 20:38:00 +0000

    modify routes

--- Junksofwood/src/pages/NotFound.jsx
+++ Junksofwood/src/pages/NotFound.jsx
@@ -1,5 +1,47 @@
-const NotFound = () => {
-    return <h1>404</h1>;
-  };
+// import { Navigate } from "react-router-dom"
+
+// const NotFound = () => {
+//     return <Navigate to="/" />;
+//   };
   
-  export default NotFound;
+//   export default NotFound;
+
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+
+//reminder: useEffect hook means "perform this side code"
+// useEffect(() => {THING THAT I WANT IT TO DO}, [WHEN I WANT IT TO DO IT])
+
+
+
+//This will navigagte to home:
+// function NotFound () {
+//   const navigate  = useNavigate()
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       navigate("/", {state : "Error Not Page"})
+//     }, 1000)
+//   }, [])
+
+//     return <h1>Page not found. Redirecting to home page.</h1>;
+//   };
+  
+//   export default NotFound;
+
+
+// This will navigate back one
+  function NotFound () {
+    const navigate  = useNavigate()
+  
+    useEffect(() => {
+      setTimeout(() => {
+        navigate(-1, {state : "Error Not Page"})
+      }, 1000)
+    }, [])
+  
+      return <h1>Page not found. Redirecting to last page.</h1>;
+    };
+    
+    export default NotFound;
