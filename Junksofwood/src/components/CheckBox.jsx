d3d4c7b49d279b3f26e697faf8033c4334526994
Gerogie18 <jennlyver@gmail.com>
2024-12-07 13:43:36 -0330

    Create Checkbox component

---
+++ Junksofwood/src/components/CheckBox.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import { MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank } from 'react-icons/md';
+
+const Checkbox = ({ text, checked, onClick }) => {
+  return (
+    <button className="checkbox-button" onClick={onClick}>
+      {checked ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />}
+      <span className="checkbox-label" style={{ paddingLeft: '0.2em' }}>{text}</span>
+    </button>
+  );
+};
+
+Checkbox.propTypes = {
+  label: PropTypes.string.isRequired, // Defines 'label' as a required string
+  checked: PropTypes.bool.isRequired, // Defines 'checked' as a required boolean
+  onClick: PropTypes.func.isRequired, // Defines 'onClick' as a required function
+};
+
+export default Checkbox;
