aa38a9876e616fc2049d2909c04fb046376b028a
Sarah Kristian Perry <sarah.perry@keyin.com>
2024-12-11 00:25:13 +0000

    adding serach function

--- Junksofwood/src/components/ProductLayout.jsx
+++ Junksofwood/src/components/ProductLayout.jsx
@@ -7,6 +7,7 @@ const ProductLayout = ({categories, products}) => {
 
   const [filteredProducts, setFilteredProducts] = useState(products);
   const [desiredCategories, setDesiredCategories] = useState([]); 
+  const [desiredTags, setDesiredTags] = useState([]); 
 
 
   const catIDs = [];
@@ -19,15 +20,18 @@ const ProductLayout = ({categories, products}) => {
       setDesiredCategories(catIDs);
     }, [categories]);
   
-    
 
-    useEffect(() => {
-    
+    useEffect(() => {  
      let newproducts = products.filter(product => desiredCategories.includes(product.category))
-      setFilteredProducts(newproducts); // Set to products or apply filtering logic
+      setFilteredProducts(newproducts);
     }, [products, desiredCategories]); // Dependency array ensures effect runs only when products change
   
+    useEffect(() => {
+      let newproducts = products.filter(product => desiredTags.includes(product.tags))
+       setFilteredProducts(newproducts); 
+     }, [desiredTags]); // Dependency array ensures effect runs only when desiredTags changes
   
+     
     function handleCheckboxChange(event, catID) {
       if (event.target.checked) {
         setDesiredCategories(prev => [...prev, catID]);
@@ -37,30 +41,61 @@ const ProductLayout = ({categories, products}) => {
    }
 
 
+   function handleTagChange(tag) {
+      setDesiredTags(prev => prev.filter(id => id !== tag));
+      //also need to remove a div
+    }
+  
+ 
+
+
+   function handleSearchChange(event) {
+    let tags = event.target.value
+      setDesiredTags(prev => [...prev, tags]);
+      return(    
+        <>
+        {desiredTags.map((tag, index) => (
+          <div key={index}>
+            <label htmlFor={tag}>{tag}</label>
+            <input 
+            type="checkbox" 
+            id={tag} 
+            name={tag} 
+            onChange={() => handleTagChange(tag)}
+            checked={desiredCategories.includes(tag)}
+            />
+          </div>
+        ))}
+      </>)
+  }
+
   return (
     <>
-        <h1>Shop</h1>
-        <div className = "categorySelection">
+      <h1>Shop</h1>
+      <div className = "categorySelection">
         <p>This should be a sidebar</p>
         {categories.map((category) => (
           <div key={category.id}>
-          <label htmlFor={category.id}>{category.title}</label>
-          <input 
-          type="checkbox" 
-          id={category.id} 
-          name={category.id} 
-          onChange={(event) => handleCheckboxChange(event, category.id)}
-          checked={desiredCategories.includes(category.id)}
-          />
+            <label htmlFor={category.id}>{category.title}</label>
+            <input 
+            type="checkbox" 
+            id={category.id} 
+            name={category.id} 
+            onChange={(event) => handleCheckboxChange(event, category.id)}
+            checked={desiredCategories.includes(category.id)}
+            />
           </div>
         ))}
-        </div>
-
         <div>
-            <Outlet context={{ filteredProducts }}/>
+          <input type="search" placeholder="Search" className="search-bar" value={tags} onChange={handleSearchChange} />
         </div>
-    </>
 
+      </div>
+
+      <div>
+            <Outlet context={{ filteredProducts }}/>
+      </div>
+    </>
   )
 };
 
