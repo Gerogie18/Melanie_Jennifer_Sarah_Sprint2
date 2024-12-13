08446d0445de704157d383966e80c5b194cd9c96
Gerogie18 <jennlyver@gmail.com>
2024-12-13 20:08:49 -0330

    hero image has sharp cornders

--- Junksofwood/src/components/homecomponents/HeroImage.jsx
+++ Junksofwood/src/components/homecomponents/HeroImage.jsx
@@ -3,8 +3,8 @@ function HeroImage(){
     return (
         <section id="hero-image">
             <div className="hero_img_container">
-                <div className="hero_img">
-                    <img
+                <div>
+                    <img className="hero-img"
                         src={"/assets/marketingimages/meghanphotoshoot/meghannewfoundland.jpg"} 
                         alt={"Image of Model in front of red and yellow building wearing Junks of wood Newfoundland Earrings"} /> Here is where we should place a Hero Image
                 </div>
