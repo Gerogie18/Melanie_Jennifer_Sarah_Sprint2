ac52531809355410bef4eb142b8eb695e183a07a
Gerogie18 <jennlyver@gmail.com>
2024-12-13 20:32:32 -0330

    fixed my fix of heroimage
    see comments on how to move crop

--- Junksofwood/src/components/homecomponents/HeroImage.jsx
+++ Junksofwood/src/components/homecomponents/HeroImage.jsx
@@ -3,8 +3,8 @@ function HeroImage(){
     return (
         <section id="hero-image">
             <div className="hero_img_container">
-                <div>
-                    <img className="hero-img"
+                <div className="hero_img">
+                    <img
                         src={"/assets/marketingimages/meghanphotoshoot/meghannewfoundland.jpg"} 
                         alt={"Image of Model in front of red and yellow building wearing Junks of Wood Newfoundland Earrings"} /> 
                 </div>
