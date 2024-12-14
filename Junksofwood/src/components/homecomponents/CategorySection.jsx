8f43779f09bb49f1a4f825aeb96eb65be9dc417f
melanie <114537207+todayisthorsday@users.noreply.github.com>
2024-12-14 19:06:11 -0330

    changes

--- Junksofwood/src/components/homecomponents/CategorySection.jsx
+++ Junksofwood/src/components/homecomponents/CategorySection.jsx
@@ -1,32 +1,57 @@
-function CategorySection(){
+import { useNavigate } from "react-router-dom";
 
-    return (
-        <section id="category_section">
-            <h1>Categories</h1>
-            <div className="descrpt"><p>Words go here, Melanie needs to think of Words<br></br>
-            Lots of words Lots of words Lots of words Lots of words Lots of words <br></br> Lots of words Lots of words Lots of words Lots of words Lots of words <br></br>Lots of words Lots of words Lots of words Lots of words</p>
-                </div>
-            <div className="category_img_container">
-                <div className="category_img">
-                    <img
-                        src={"../assets/productthumbnails/brooches/PuffinBroochPlate2.jpg_thumbnail.jpg"} 
-                        alt={"product image"} >
-                    </img>
-                </div>
-                <div className="category_img">
-                    <img
-                        src={"../assets/productthumbnails/earrings/MoonkittiesEarringCard.jpg_thumbnail.jpg"} 
-                        alt={"product image"} >
-                    </img>
-                </div>
-                <div className="category_img">
-                    <img
-                        src={"../assets/productthumbnails/keychains/RottedKeychainPlateTag.jpg_thumbnail.jpg"} 
-                        alt={"product image"} >
-                    </img>
-                </div>
-            </div>
-        </section>)
+function MyButton() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/shop");
+  };
+
+  return <button onClick={handleButtonClick}>Shop all</button>;
 }
 
-export default CategorySection
+function CategorySection() {
+  return (
+    <section id="category_section">
+      <h1>Categories</h1>
+      <div className="descrpt">
+        <p>
+          Words go here, Melanie needs to think of Words<br></br>
+          Lots of words Lots of words Lots of words Lots of words Lots of words{" "}
+          <br></br> Lots of words Lots of words Lots of words Lots of words Lots
+          of words <br></br>Lots of words Lots of words Lots of words Lots of
+          words
+        </p>
+        <MyButton />
+      </div>
+      <div className="category_img_container">
+        <div className="category_img">
+          <img
+            src={
+              "../assets/productthumbnails/brooches/PuffinBroochPlate2.jpg_thumbnail.jpg"
+            }
+            alt={"product image"}
+          ></img>
+        </div>
+        <div className="category_img">
+          <img
+            src={
+              "../assets/productthumbnails/earrings/MoonkittiesEarringCard.jpg_thumbnail.jpg"
+            }
+            alt={"product image"}
+          ></img>
+        </div>
+        <div className="category_img">
+          <img
+            src={
+              "../assets/productthumbnails/keychains/RottedKeychainPlateTag.jpg_thumbnail.jpg"
+            }
+            alt={"product image"}
+          ></img>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default CategorySection;
--- Junksofwood/src/components/homecomponents/LatestArrivals.jsx
+++ Junksofwood/src/components/homecomponents/LatestArrivals.jsx
@@ -1,33 +1,58 @@
-function LatestArrivals(){
+import { useNavigate } from "react-router-dom";
 
-    return (
-        <section id="latest_arrivals">
-            <h1>Latest Arrivals</h1>
- <div className="descrpt"><p>Words go here, Melanie needs to think of Words<br></br>
-            Lots of words Lots of words Lots of words Lots of words Lots of words <br></br> Lots of words Lots of words Lots of words Lots of words Lots of words <br></br>Lots of words Lots of words Lots of words Lots of words</p>
-            </div>
-            
-            <div className="arrivals_img_container">
-                <div className="arrivals_img">
-                    <img
-                        src={"../assets/productthumbnails/ornaments/TibbsOrnamentPlate.jpg_thumbnail.jpg"} 
-                        alt={"product image"} />
-                    
-                </div>
-                <div className="arrivals_img">
-                    <img
-                        src={"../assets/productthumbnails/ornaments/MummersOrnamentPlate.jpg_thumbnail.jpg"} 
-                        alt={"product image"} >
-                    </img>
-                </div>
-                <div className="arrivals_img">
-                    <img
-                        src={"../assets/productthumbnails/ornaments/YesbyOrnamentPlate.jpg_thumbnail.jpg"} 
-                        alt={"product image"} >
-                    </img>
-                </div>
-            </div>
-        </section>)
+function MyButton() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/shop");
+  };
+
+  return <button onClick={handleButtonClick}>Shop all</button>;
 }
 
-export default LatestArrivals
+function LatestArrivals() {
+  return (
+    <section id="latest_arrivals">
+      <h1>Our Latest Arrivals</h1>
+      <div className="descrpt">
+        <p>
+          Words go here, Melanie needs to think of Words<br></br>
+          Lots of words Lots of words Lots of words Lots of words Lots of words{" "}
+          <br></br> Lots of words Lots of words Lots of words Lots of words Lots
+          of words <br></br>Lots of words Lots of words Lots of words Lots of
+          words
+        </p>
+        <MyButton />
+      </div>
+
+      <div className="arrivals_img_container">
+        <div className="arrivals_img">
+          <img
+            src={
+              "../assets/productthumbnails/ornaments/TibbsOrnamentPlate.jpg_thumbnail.jpg"
+            }
+            alt={"product image"}
+          />
+        </div>
+        <div className="arrivals_img">
+          <img
+            src={
+              "../assets/productthumbnails/ornaments/MummersOrnamentPlate.jpg_thumbnail.jpg"
+            }
+            alt={"product image"}
+          ></img>
+        </div>
+        <div className="arrivals_img">
+          <img
+            src={
+              "../assets/productthumbnails/ornaments/YesbyOrnamentPlate.jpg_thumbnail.jpg"
+            }
+            alt={"product image"}
+          ></img>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default LatestArrivals;
