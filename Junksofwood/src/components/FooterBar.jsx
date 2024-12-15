ed56e03005ea973af78b164a39dfaeca857bec75
Gerogie18 <jennlyver@gmail.com>
2024-12-15 07:03:15 -0330

    Update FooterBar.jsx

--- Junksofwood/src/components/FooterBar.jsx
+++ Junksofwood/src/components/FooterBar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
 // import { FaRegCopyright } from "react-icons/fa6";
 import { PiCopyright } from "react-icons/pi";
 
@@ -9,9 +8,6 @@ const FooterBar = () => {
       <span>
         <PiCopyright /> Copyright 2024 Junks of Wood. All Rights Reserved.
       </span>
-      <NavLink to="/test" style={{textdecoration: 'none'}}>
-        <button  style={{padding: '5px,', marginLeft: "20px"}}>Test</button>
-      </NavLink>
     </footer>
   )
 }
